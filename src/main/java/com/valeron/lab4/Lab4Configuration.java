d78f881cf3fc049058fadc806906569d0a44a173
Valeron93 <valera1800@gmail.com>
2022-11-28 18:41:55 +0100

    added EuclidSolver bean

--- src/main/java/com/valeron/lab4/Lab4Configuration.java
+++ src/main/java/com/valeron/lab4/Lab4Configuration.java
@@ -1,6 +1,8 @@
 package com.valeron.lab4;
 
 import com.valeron.lab4.dao.*;
+import com.valeron.lab4.model.*;
+import com.valeron.lab4.solver.*;
 import org.springframework.context.annotation.*;
 
 @Configuration
@@ -8,8 +10,14 @@ public class Lab4Configuration {
 
     @Bean
     @Scope("singleton")
-    public SimpleRepository simpleRepository() {
+    public SimpleRepository<SolveResult> solveResultRepository() {
         return new FileRepository();
+    }
+
+    @Bean
+    @Scope("singleton")
+    public EuclidSolver euclidSolver() {
+        return new EuclidSolverImpl();
     }
 
 }
